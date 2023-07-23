54e7966b74b71ee414361bb00984a25ea4091469
maheshkale96 <maheshkale11196@gmail.com>
2023-07-24 00:21:05 +0530

    findByFromAndTo menthod

--- src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -8,18 +8,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+
 @RestController
 public class CurrencyExchangeController {
 
 	@Autowired
 	private Environment environment;
 	
+	@Autowired
+	private CurrencyExchangeRepository currencyExchangeRepository;
+	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
 		
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
+//		CurrencyExchange currencyExchange = new CurrencyExchange(1000L,from,to,BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
 		
+		if(currencyExchange==null) {
+			throw new RuntimeException("Unable to Find data for "+ from + " to "+ to);
+		}
 		String port = environment.getProperty("local.server.port");
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;
