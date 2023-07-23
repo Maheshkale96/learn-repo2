0d35f412b1d50ae39b51c8d9b4ed1f80e5a9d8db
maheshkale96 <maheshkale11196@gmail.com>
2023-07-23 20:12:46 +0530

    JPA for database

--- src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchange.java
+++ src/main/java/com/microservices/currencyexchangeservice/controller/CurrencyExchange.java
@@ -2,9 +2,20 @@ package com.microservices.currencyexchangeservice.controller;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
+	
+	@Id
 	private Long id;
+	
+	@Column(name="currency_from")
 	private String from;
+	
+	@Column(name="currency_to")
 	private String to;
 	private BigDecimal conversionMultiple;
 	private String environment;
